13af7dbfa9c635aae0da5e7506ac8d71bc1f6990
xiaohuoni <448627663@qq.com>
2018-04-27 12:58:56 +0800

    提交mock服务器

---
+++ README.md
@@ -0,0 +1,9 @@
+# Antd-Pro-Serve
+antd-pro 的线上mock数据，为了build之后的展示。
+
+## start
+```base
+git clone  https://github.com/xiaohuoni/antd-pro-server.git
+npm i 
+node app.js
+```
---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var morgan = require('morgan')
+var proxyData = require("./proxy");
+var app = express();
+app.use(morgan('combined'))
+app.use((req, res) => {
+  var result = proxyData[req.method][req.url];
+  if (Object.prototype.toString.call(result) === "[object String]"||Object.prototype.toString.call(result) === "[object Array]") {
+    res.json(result)
+  } else if (Object.prototype.toString.call(result) === "[object Function]") {
+    result(req, res);
+  }
+});
+var server = app.listen(3000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("Example app listening at http://%s:%s", host, port);
+});
